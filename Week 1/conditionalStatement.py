7f797ad962c8536236ae9722691fc17947b1b121
towfiqueHaider <160301263+towfiqueHaider@users.noreply.github.com>
2025-01-15 10:48:14 -0800

    Add files via upload

---
+++ Week 1/conditionalStatement.py
@@ -0,0 +1,14 @@
+# Task 1
+x = float(input("Enter cgpa of friend 1: "))
+y = float(input("Enter cgpa of friend 2: "))
+z = float(input("Enter cgpa of friend 3: "))
+
+if x < y and x < z:
+    print("Friend 1 has the lowest cgpa")
+
+elif y < z and y < z:
+    print("Friend 2 has the lowest cgpa")
+
+else:
+    print("Friend 3 has the lowest cgpa")
+
---
+++ Week 1/f_String.py
@@ -0,0 +1,8 @@
+# Task 2 - Average cgpa of 3 friends
+x = float(input("Enter cgpa of friend 1: "))
+y = float(input("Enter cgpa of friend 2: "))
+z = float(input("Enter cgpa of friend 3: "))
+
+avg = float((x+y+z)/3)
+
+print(f"The average cgpa of three friends are: {avg}")
